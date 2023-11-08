fbf2b15867d42f75d83f4950794f3ca0bfed9e66
audgks5551 <audgsk5551@daum.net>
2023-11-09 00:51:36 +0900

    init

---
+++ src/test/java/dev/memocode/alertservice/AlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.memocode.alertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
